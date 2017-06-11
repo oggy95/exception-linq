48a88056b2eec733fbb8d422cb429500e575f7c6
oggy95 <gorobchuk95@gmail.com>
2017-06-11 12:53:45 +0300

    commit

---
+++ EXCEPTION+LINQ/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXCEPTION_LINQ
+{
+    class Program
+    {
+        static char[] separators = { ' ', ',', '.', ':', '\t' };
+        public іефешс string GetLongest(IEnumerable<string> words)
+        {
+            string word;
+            word = words
+                    .Where(s => s.Length == words.Max(w => w.Length))
+                    .Min(s => s)
+                    .ToString();
+            try
+            {                
+                int number = Int32.Parse(word);
+                Console.WriteLine("It's a number!");
+                return number.ToString();
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("It's not a number!");
+                return word;
+            }
+            
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please write your strings");
+            string strings = Console.ReadLine();
+            string[] stringArray = strings.Split(separators);
+            Console.WriteLine(GetLongest(stringArray));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ EXCEPTION+LINQ/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("EXCEPTION+LINQ")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EXCEPTION+LINQ")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("cb4a7f4d-f914-4897-96db-b52f9388ff13")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//      Номер построения
+//      Редакция
+//
+// Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
